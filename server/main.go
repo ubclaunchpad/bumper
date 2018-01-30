031c62b0e57c41b624d11d95735f7a70fd44f517
Brian Nguyen <briannguyen992@gmail.com>
2018-01-30 10:43:54 -0800

    Bare http server

---
+++ server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello from Go!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	log.Println("Starting server on localhost:9090")
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("Error starting server")
+	}
+}

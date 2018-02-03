b91ef8af7e1f673aa03f7f8fafa0ee76f6be6652
Brian Nguyen <briannguyen992@gmail.com>
2018-02-03 15:19:51 -0800

    Goroutine to ping all clients

--- server/main.go
+++ server/main.go
@@ -20,7 +20,9 @@ var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan Message)
 
 // this 'upgrades' a normal HTTP connection to a persistent TCP connection (socket)
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true }
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello from Go!")
@@ -44,6 +46,7 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 		// terminate connection if error occurs
 		if err != nil {
 			log.Printf("error: %v", err)
+			client.Close()
 			delete(clients, ws)
 			break
 		}
@@ -54,11 +57,28 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 }
 
 func tick() {
-
+	tickCount := 0
+	for {
+		time.Sleep(time.Second * 5)
+		msg := Message{"tick" + strconv.Itoa(tickCount)}
+		// update every client
+		for client := range clients {
+			err := client.WriteJSON(&msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+		tickCount += 1
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	// main thread that will listen for connections
+	http.HandleFunc("/connect", handleConnection)
+	// separate thread that will handle updates
+	go tick()
 
 	log.Println("Starting server on localhost:9090")
 	err := http.ListenAndServe(":9090", nil)

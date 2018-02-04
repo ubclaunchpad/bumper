a03df67a98aebf35a9c464aa0280a0ef8c79504a
Amanda Lazar <amandalazar@shaw.ca>
2018-02-04 14:42:46 -0800

    Classes almost finished

--- Components/Hole.js
+++ Components/Hole.js
@@ -1,14 +1,38 @@
-import React from 'react';
-
-export default class Hole extends React.Component {
+class Hole {
+  var position;
+  var radius;
+  var lifespan;
+  
+  // Note: Lifespan must be in milliseconds (for use in setTimeout)
   constructor(position, radius, lifespan) { 
-    this.position = position;
 	this.radius = radius;
 	this.lifespan = lifespan;
-  }
-
-  move() {
-	  
+	
+	// Assuming a spawnHole function exists with this parameter
+	// and returns the position the hole is placed
+	this.position = spawnHole(radius); 
+	
+	var timerFlag = 0;
+	var timer = setTimeout(timer,lifespan); // timerFlag = 1 after lifespan milliseconds
+	drawHole();
   }
   
+  // Continuously redraws the hole on each frame
+  drawHole() {
+	  while(timerFlag === 0) {
+		  // CODE TO CLEAR CANVAS AND DRAW HOLE HERE
+		  requestAnimationFrame(draw);
+	  }
+  }
+  
+  timer() {
+	  timerFlag = 1;
+  }
+  
+  
+  // Getter method to be used to detect if a player/junk is within
+  // the bounds of a hole
+  getPositionAndRadius() {
+	  return {position, radius};
+  }
 }
--- Components/Junk.js
+++ Components/Junk.js
@@ -1,16 +1,34 @@
-import React from 'react';
-
-export default class Junk extends React.Component {
-  constructor(mass, position, velocity, pointVal, lastBumped) {
+class Junk {
+	var velocity = { 0.0, 0.0 };
+	var lastBumped = undefined;
+	var mass;
+	var position;
+	var pointVal;
+	var alive = true; // NEED TO CHANGE TO FALSE IF JUNK FALLS INTO THE AREA OF A HOLE, ETC.
+	
+  constructor(mass, pointVal) {
     this.mass = mass;
-	this.position = position;
-	this.velocity = velocity;
 	this.pointVal = pointVal;
-	this.lastBumped = lastBumped;
+	
+	// Assuming a spawnJunk function exists with this parameter
+	// and returns the position the hole is placed
+	this.position = spawnJunk();
+	
+	move();
   }
 
-  move() {
-	  
+  move() { // Assuming velocity instance variable is updated if a collision occurs
+	 var stationary = (this.velocity === { 0.0, 0.0 });
+	 if(!stationary) { // Move junk based on velocity
+		// Calculate new position
+		// ADD CODE FOR CALCULATING NEW POSITION BASED ON THIS.POSITION AND VELOCITY
+		// PUT NEW POSITION IN THIS.POSITION
+	  }
+	  // Draw new position
+	  // ADD CODE FOR DRAWING NEW POSITION
+	  if(alive) {
+		requestAnimationFrame(move); // Continue looping move() if alive
+	  }
   }
   
 }

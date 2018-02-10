fc29c928fa3645cc494b852961bb9cf3c846121c
SeeratSek <seeratsek@gmail.com>
2018-02-10 11:45:31 -0800

    fix linting errors

--- client/App.js
+++ client/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 const PLAYER_RADIUS = 20;
 const JUNK_COUNT = 5;
 const JUNK_SIZE = 15;
-const HOLE_COUNT = 10;
+const HOLE_COUNT = 5;
 const HOLE_RADIUS = 25;
 const MAX_RADIUS = 50;
 
-var width = window.innerWidth;
-var height = window.innerHeight;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 export default class App extends React.Component {
   constructor(props) {
@@ -17,7 +17,7 @@ export default class App extends React.Component {
       allCoords: [],
       junkCoords: [],
       holeCoords: [],
-      playerCoords: []
+      playerCoords: [],
     };
     this.drawObjects = this.drawObjects.bind(this);
   }
@@ -29,84 +29,81 @@ export default class App extends React.Component {
   }
 
   generateJunkCoordinates() {
-    var newCoords = this.generateCoords(JUNK_COUNT);
-    this.setState({ junkCoords: newCoords }); 
+    const newCoords = this.generateCoords(JUNK_COUNT);
+    this.setState({ junkCoords: newCoords });
   }
 
   generateHoleCoordinates() {
-    var newCoords = this.generateCoords(HOLE_COUNT); 
+    const newCoords = this.generateCoords(HOLE_COUNT);
     this.setState({
-      holeCoords: newCoords
+      holeCoords: newCoords,
     });
   }
 
-  // should appear somewhere in the centre 
+  // should appear somewhere in the centre
   generatePlayerCoordinates() {
-    var maxWidth = (2*width)/3;
-    var minWidth = width/3;
-    var maxHeight = (2*height)/3;
-    var minHeight = height/3
-    var x = Math.floor(Math.random() * (maxWidth - minWidth + 1)) + minWidth;
-    var y = Math.floor(Math.random() * (maxHeight - minHeight + 1)) + minHeight;
-    this.setState({ playerCoords: { x: x, y: y} });
+    const maxWidth = (2 * width) / 3;
+    const minWidth = width / 3;
+    const maxHeight = (2 * height) / 3;
+    const minHeight = height / 3;
+    const x = Math.floor(Math.random() * ((maxWidth - minWidth) + 1)) + minWidth;
+    const y = Math.floor(Math.random() * ((maxHeight - minHeight) + 1)) + minHeight;
+    this.setState({ playerCoords: { x, y } });
   }
 
   generateCoords(num) {
-    var coords = [];
-    while(num > 0) {
-      var x = Math.floor(Math.random()*(width-MAX_RADIUS));
-      var y = Math.floor(Math.random()*(height-MAX_RADIUS));
-      var placed = true;
+    let count = num;
+    const coords = [];
+    while (count > 0) {
+      const x = Math.floor(Math.random() * (width - MAX_RADIUS));
+      const y = Math.floor(Math.random() * (height - MAX_RADIUS));
+      let placed = true;
 
       // check whether area is available
-      for (var point of this.state.allCoords) {
+      for (const p of this.state.allCoords) { //es-lint-disable no-restricted-syntax 
         // could not be placed because of overlap
-        if (Math.abs(point.x-x) < MAX_RADIUS || Math.abs(point.y-y) < MAX_RADIUS) {
+        if (Math.abs(p.x - x) < MAX_RADIUS || Math.abs(p.y - y) < MAX_RADIUS) {
           placed = false;
           break;
         }
       }
 
       if (placed) {
-        var newAllCoords = this.state.allCoords.push({ x: x, y: y });
-        this.setState({ allCoords: newAllCoords});
-        coords.push({ x: x, y: y });
-        num = num -1;
+        const newAllCoords = this.state.allCoords.push({ x, y });
+        this.setState({ allCoords: newAllCoords });
+        coords.push({ x, y });
+        count -= 1;
       }
     }
     return coords;
   }
 
   drawObjects() {
-
-    var canvas = document.getElementById("ctx");
-    var ctx = canvas.getContext("2d");
-    for (var point of this.state.junkCoords) {
+    const canvas = document.getElementById('ctx');
+    const ctx = canvas.getContext('2d');
+    for (const p of this.state.junkCoords) {
       ctx.beginPath();
-      ctx.rect(point.x, point.y, JUNK_SIZE, JUNK_SIZE);
-      ctx.fillStyle = "white";
+      ctx.rect(p.x, p.y, JUNK_SIZE, JUNK_SIZE);
+      ctx.fillStyle = 'white';
       ctx.fill();
       ctx.closePath();
     }
-    for (var point of this.state.holeCoords) {
+    for (const p of this.state.holeCoords) {
       ctx.beginPath();
-      ctx.arc(point.x, point.y, HOLE_RADIUS, 0, Math.PI*2);
-      ctx.fillStyle = "white";
+      ctx.arc(p.x, p.y, HOLE_RADIUS, 0, Math.PI * 2);
+      ctx.fillStyle = 'white';
       ctx.fill();
       ctx.closePath();
     }
 
     ctx.beginPath();
-    ctx.arc(this.state.playerCoords.x, this.state.playerCoords.y, PLAYER_RADIUS, 0, Math.PI*2);
-    ctx.fillStyle = "green";
+    ctx.arc(this.state.playerCoords.x, this.state.playerCoords.y, PLAYER_RADIUS, 0, Math.PI * 2);
+    ctx.fillStyle = 'green';
     ctx.fill();
     ctx.closePath();
-    
   }
-
   componentDidUpdate() {
     this.drawObjects();
-
   }
 
   render() {

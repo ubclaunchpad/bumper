7193645d49ab75620fc0c8cb6c23648ad54f2a56
Tommy Lau <tommylau94@gmail.com>
2018-02-09 19:37:47 -0800

    added client websocket constructor

--- client/App.js
+++ client/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+const address = 'ws://localhost:9090/connect'
+
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    if(window.WebSocket){
+      this.socket = new WebSocket(address);
+      this.socket.onmessage = (event) => console.log(event.data);
+    }
+    else{
+      console.log('websocket not available');
+    }
+    
+
     this.state = {};
   }
 

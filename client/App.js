c58bae68fa4df8d786f16d0b49ce6aeffd67c4de
Brian Nguyen <briannguyen992@gmail.com>
2018-02-10 11:30:52 -0800

    Style fix

--- client/App.js
+++ client/App.js
@@ -6,16 +6,13 @@ const address = 'ws://localhost:9090/connect'
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-
-    if(window.WebSocket){
+    if (window.WebSocket) {
       this.socket = new WebSocket(address);
-      this.socket.onmessage = (event) => console.log(event.data);
-    }
-    else{
+      this.socket.onmessage = event => console.log(event.data);
+    } else {
       console.log('websocket not available');
     }
     
-
     this.state = {};
   }
 

cfde5a9f515ae6a1126914df3456b926deb5c103
Brian Nguyen <briannguyen992@gmail.com>
2018-02-17 11:51:24 -0800

    Refactor class

--- client/components/Hole.js
+++ client/components/Hole.js
@@ -1,31 +1,20 @@
 export default class Hole {
   // Note: Lifespan must be in milliseconds (for use in setTimeout)
-  constructor({ position, radius, lifespan, canvas }) { 
-	this.radius = radius;
-	this.lifespan = lifespan;
-	this.position = generateHoleCoordinates(); // Need to change this function to return position array, or take this line out and keep as is
-	
-	var timerFlag = 0;
-	var timer = setTimeout(timer,lifespan); // timerFlag = 1 after lifespan milliseconds
-	drawHole();
+  constructor(props) {
+    this.canvas = props.canvas;
+    this.radius = props.radius;
+    this.lifespan = props.lifespan;
+    this.position = {};
+
+    this.getPositionAndRadius = this.getPositionAndRadius.bind(this);
+    this.drawHole = this.drawHole.bind(this);
   }
-  
-  // Continuously redraws the hole on each frame
-  drawHole() {
-	  while(timerFlag === 0) {
-		  // CODE TO CLEAR CANVAS AND DRAW HOLE HERE
-		  requestAnimationFrame(draw);
-	  }
-  }
-  
-  timer() {
-	  timerFlag = 1;
-  }
-  
-  // Getter method to be used to detect if a player/junk is within
-  // the bounds of a hole
+
   getPositionAndRadius() {
-	  return { position, radius };
+    return { position: this.position, radius: this.radius };
   }
-  
+
+  drawHole() {
+
+  }
 }

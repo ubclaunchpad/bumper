dc4aace86c821421910943fdb9db7102bb83249b
Brian Nguyen <briannguyen992@gmail.com>
2018-02-17 14:58:14 -0800

    Update Player.js

--- client/components/Player.js
+++ client/components/Player.js
@@ -1,11 +1,11 @@
 export default class Player {
 	constructor(props) { 
 		this.mass = props.mass || 10;
-		this.color = props.color || #FF0000; // Red 
-		this.name = props.name || "Default name";
+		this.color = props.color || '#FF0000'; // Red 
+		this.name = props.name || 'Default name';
 		this.canvas = props.canvas;
-		this.position = props.position || {x: (window.innerWidth / 2), y: (window.innerHeight) / 2)}; 
-		this.velocity = {dx: 0, dy: 0};
+		this.position = props.position || { x: (window.innerWidth / 2), y: (window.innerHeight) / 2) }; 
+		this.velocity = { dx: 0, dy: 0 };
 		this.alive = true;  
 		
 		this.drawPlayer = this.drawPlayer.bind(this);

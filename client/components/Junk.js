85c67349a8680d95e341236f42e49156c9b4a8fa
Amanda Lazar <amandalazar@shaw.ca>
2018-02-17 14:55:32 -0800

    Player and junk classes revised

--- client/components/Junk.js
+++ client/components/Junk.js
@@ -1,9 +1,9 @@
 export default class Junk {
   constructor(props) {
-    this.mass = props.mass;
-	this.pointVal = props.pointVal;
-	this.position = props.position;
-	this.velocity = [0, 0];
+    this.mass = props.mass || 10;
+	this.pointVal = props.pointVal || 50;
+	this.position = props.position || {x: (window.innerWidth / 2), y: (window.innerHeight) / 2)};
+	this.velocity = {dx: 0, dy: 0};
 	this.lastBumped = null;
 	this.alive = true;
 	
--- client/components/Player.js
+++ client/components/Player.js
@@ -1,12 +1,12 @@
 export default class Player {
 	constructor(props) { 
-		this.mass = props.mass;
-		this.color = props.color; 
-		this.name = props.name;
+		this.mass = props.mass || 10;
+		this.color = props.color || #FF0000; // Red 
+		this.name = props.name || "Default name";
 		this.canvas = props.canvas;
-		this.position = props.position;
-		this.velocity = [0, 0];
-		this.alive = true; 
+		this.position = props.position || {x: (window.innerWidth / 2), y: (window.innerHeight) / 2)}; 
+		this.velocity = {dx: 0, dy: 0};
+		this.alive = true;  
 		
 		this.drawPlayer = this.drawPlayer.bind(this);
 	 }

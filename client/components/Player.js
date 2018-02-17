a6cecd37512e8979b404c60c5a21009bd7a67c1b
Brian Nguyen <briannguyen992@gmail.com>
2018-02-17 15:46:07 -0800

    Moving to player class

--- client/components/Player.js
+++ client/components/Player.js
@@ -1,17 +1,45 @@
+const PLAYER_RADIUS = 25;
+
 export default class Player {
-	constructor(props) { 
-		this.mass = props.mass || 10;
-		this.color = props.color || '#FF0000'; // Red 
-		this.name = props.name || 'Default name';
-		this.canvas = props.canvas;
-		this.position = props.position || { x: (window.innerWidth / 2), y: (window.innerHeight) / 2) }; 
-		this.velocity = { dx: 0, dy: 0 };
-		this.alive = true;  
-		
-		this.drawPlayer = this.drawPlayer.bind(this);
-	 }
-		
-	drawPlayer() {
-		
-	}
+  constructor(props) {
+    this.canvas = props.canvas;
+    this.position = props.position || { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+    this.velocity = { dx: 0, dy: 0 };
+    this.theta = props.theta;
+
+    this.mass = props.mass || 10;
+    this.color = props.color || '#FF0000';
+    this.name = props.name || 'Default name';
+    this.alive = true;
+
+    this.drawPlayer = this.drawPlayer.bind(this);
+  }
+
+  drawPlayer() {
+    const ctx = this.canvas.getContext('2d');
+    const { theta } = this.state;
+    const { x, y } = this.state.position;
+    ctx.beginPath();
+    ctx.arc(x, y, PLAYER_RADIUS, 0, Math.PI * 2);
+    ctx.fillStyle = '#00FFFF';
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(x + (PLAYER_RADIUS * Math.sin(theta)), y + (PLAYER_RADIUS * Math.cos(theta)));
+    ctx.lineTo(x - (PLAYER_RADIUS * Math.sin(theta)), y - (PLAYER_RADIUS * Math.cos(theta)));
+    ctx.strokeStyle = '#000000';
+    ctx.strokeWidth = 5;
+    ctx.stroke();
+
+    const backCenterX = x - ((PLAYER_RADIUS * Math.sin(theta)) / 2);
+    const backCenterY = y - ((PLAYER_RADIUS * Math.cos(theta)) / 2);
+    const backLength = (2.5 * ((PLAYER_RADIUS / 2) / Math.tan(45)));
+    ctx.beginPath();
+    ctx.moveTo(backCenterX - (backLength * Math.cos(theta)), backCenterY + (backLength * Math.sin(theta)));
+    ctx.lineTo(backCenterX + (backLength * Math.cos(theta)), backCenterY - (backLength * Math.sin(theta)));
+    ctx.strokeStyle = '#0000000';
+    ctx.strokeWidth = 5;
+    ctx.stroke();
+  }
 }

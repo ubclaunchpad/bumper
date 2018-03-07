b30860a35a965230c393a553f888e6f6a4138b54
Brian Nguyen <briannguyen992@gmail.com>
2018-03-06 17:56:14 -0800

    Deploying last commit

--- client/public/bundle.js
+++ client/public/bundle.js
@@ -2845,7 +2845,7 @@ var MAX_HOLE_LIFE = 75;
 
 var width = window.innerWidth;
 var height = window.innerHeight;
-var address =  true ? 'ws://ec2-18-218-38-25.us-east-2.compute.amazonaws.com:9090/connect' : 'ws://localhost:9090/connect';
+var address =  true ? 'ws://ec2-18-188-53-231.us-east-2.compute.amazonaws.com:9090/connect' : 'ws://localhost:9090/connect';
 
 // detect collision
 // (x2-x1)^2 + (y1-y2)^2 <= (r1+r2)^2

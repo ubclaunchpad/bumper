d952d22529b51b9105af52029412c8f3ca923da3
Tommy Lau <tommylau94@gmail.com>
2018-03-09 20:13:44 -0800

    Create Arena Model

--- server/game/game.go
+++ server/game/game.go
@@ -1,1 +1,23 @@
 package game
+
+import (
+	"github.com/ubclaunchpad/bumper/server/models"
+)
+
+// Arena container for play area information including all objects
+type Arena struct {
+	Height  int // Height of play area in pixels
+	Width   int // Width of play area in pixels
+	Holes   []models.Hole
+	Junk    []models.Junk
+	Players []models.Player
+}
+
+// createArena constructor for arena to initialize a width/height
+// returns an Arena struct with width and height
+func createArena(height int, width int) *Arena {
+	a := new(Arena)
+	a.Height = height
+	a.Width = width
+	return a
+}

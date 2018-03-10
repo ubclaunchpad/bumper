58df542af11b82029e78129b04ef94e447d2d37f
Graeme <rennie_3629@hotmail.com>
2018-03-10 10:16:15 -0800

    add hole class, fixed float32s

--- server/models/models.go
+++ server/models/models.go
@@ -16,22 +16,16 @@ type Velocity struct {
 	Dy float64 `json:"dy"`
 }
 
-// Hole contains the data for a hole's position and size
-type Hole struct {
-	Position Position `json:"position"`
-	Radius   float64  `json:"radius"`
-}
-
 func (v *Velocity) magnitude() float64 {
-	return math.Sqrt(float64((v.Dx * v.Dx) + (v.Dy * v.Dy)))
+	return math.Sqrt(v.Dx*v.Dx + v.Dy*v.Dy)
 }
 
 func (v *Velocity) normalize() *Velocity {
 	var mag = v.magnitude()
 	if mag > 0 {
 		return &Velocity{
-			Dx: v.Dx / float32(mag),
-			Dy: v.Dy / float32(mag),
+			Dx: v.Dx / mag,
+			Dy: v.Dy / mag,
 		}
 	}
 

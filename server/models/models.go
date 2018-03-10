8163b98887ed21788c17d69d3a835d4d3291b26d
Graeme <rennie_3629@hotmail.com>
2018-03-10 10:11:26 -0800

    added junk model

--- server/models/models.go
+++ server/models/models.go
@@ -6,27 +6,20 @@ import (
 
 // Position x y position
 type Position struct {
-	X float32 `json:"x"`
-	Y float32 `json:"y"`
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
 }
 
 // Velocity dx dy velocity
 type Velocity struct {
-	Dx float32 `json:"dx"`
-	Dy float32 `json:"dy"`
+	Dx float64 `json:"dx"`
+	Dy float64 `json:"dy"`
 }
 
 // Hole contains the data for a hole's position and size
 type Hole struct {
 	Position Position `json:"position"`
-	Radius   float32  `json:"radius"`
-}
-
-// Junk a position and velocity struct describing it's state and player struct to identify rewarding points
-type Junk struct {
-	Position Position `json:"position"`
-	Velocity Velocity `json:"velocity"`
-	Player   Player   `json:"player"`
+	Radius   float64  `json:"radius"`
 }
 
 func (v *Velocity) magnitude() float64 {

64137ab4ad6052afd569b83802b654faa951af46
Brian Nguyen <briannguyen992@gmail.com>
2018-03-09 19:29:14 -0800

    Sending and logging state

--- server/main.go
+++ server/main.go
@@ -99,19 +99,35 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 }
 
 func tick() {
+	players := []models.Player{
+		models.Player{ID: 1, Position: models.Position{X: 200, Y: 200}, Velocity: models.Velocity{Dx: 0, Dy: 0}, Color: "blue"},
+		models.Player{ID: 2, Position: models.Position{X: 250, Y: 250}, Velocity: models.Velocity{Dx: 0, Dy: 0}, Color: "red"},
+		models.Player{ID: 3, Position: models.Position{X: 300, Y: 300}, Velocity: models.Velocity{Dx: 0, Dy: 0}, Color: "green"},
+	}
+	holes := []models.Hole{
+		models.Hole{Position: models.Position{X: 150, Y: 150}, Radius: 10},
+		models.Hole{Position: models.Position{X: 100, Y: 100}, Radius: 10},
+	}
+	junk := []models.Junk{
+		models.Junk{Position: models.Position{X: 50, Y: 50}, Velocity: models.Velocity{Dx: 0, Dy: 0}},
+		models.Junk{Position: models.Position{X: 25, Y: 25}, Velocity: models.Velocity{Dx: 0, Dy: 0}},
+	}
 	for {
 		time.Sleep(time.Millisecond * 17) // 60 Hz
-		var objectarray []ObjectState
-		msg := ServerState{Type: "update"}
-
-		for client := range clients {
-			objectarray = append(objectarray, *clients[client])
+		serverState := struct {
+			Type    string          `json:"type"`
+			Players []models.Player `json:"players"`
+			Holes   []models.Hole   `json:"holes"`
+			Junk    []models.Junk   `json:"junk"`
+		}{
+			"update",
+			players,
+			holes,
+			junk,
 		}
-
-		msg.Players = objectarray
 		// update every client
 		for client := range clients {
-			err := client.WriteJSON(&msg)
+			err := client.WriteJSON(&serverState)
 			if err != nil {
 				log.Printf("error: %v", err)
 				client.Close()

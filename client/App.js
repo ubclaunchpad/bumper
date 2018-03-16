3281bc9548d6e52e1c222e3c79a1f0c50742827b
Brian Nguyen <briannguyen992@gmail.com>
2018-03-16 14:41:32 -0700

    Removed unused functions

--- client/App.js
+++ client/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
-import Player from './components/Player';
-import Hole from './components/Hole';
-import Junk from './components/Junk';
 
 const PLAYER_RADIUS = 25;
-const JUNK_COUNT = 10;
 const JUNK_SIZE = 15;
-const HOLE_COUNT = 10;
-const MAX_DISTANCE_BETWEEN = 50;
-const POINTS_PER_JUNK = 100;
 
-const MIN_HOLE_RADIUS = 15;
-const MAX_HOLE_RADIUS = 30;
-const MIN_HOLE_LIFE = 25;
-const MAX_HOLE_LIFE = 75;
-
-const width = window.innerWidth;
-const height = window.innerHeight;
 const address = process.env.NODE_ENV === 'production'
   ? 'ws://ec2-18-188-53-231.us-east-2.compute.amazonaws.com:9090/connect'
   : 'ws://localhost:9090/connect';
-
-// detect collision
-// (x2-x1)^2 + (y1-y2)^2 <= (r1+r2)^2
-function areCirclesColliding(p, r1, q, r2) {
-  return (((p.x - q.x) ** 2) + ((p.y - q.y) ** 2)) <= ((r1 + r2) ** 2);
-}
 
 export default class App extends React.Component {
   constructor(props) {
@@ -41,13 +21,11 @@ export default class App extends React.Component {
     }
 
     this.state = {
-      allCoords: [],
       isInitialized: false,
       junk: null,
       holes: null,
       players: null,
       player: null,
-      points: 0,
     };
 
     this.handleMessage = this.handleMessage.bind(this);
@@ -122,7 +100,7 @@ export default class App extends React.Component {
     // eslint-disable-next-line
     requestAnimationFrame(this.tick);
   }
-  
+
   drawHoles() {
     this.state.holes.forEach((h) => {
       const ctx = this.canvas.getContext('2d');
@@ -156,7 +134,7 @@ export default class App extends React.Component {
       ctx.closePath();
     });
   }
-  
+
   drawPlayerPoints() {
     const ctx = this.canvas.getContext('2d');
     ctx.beginPath();
@@ -190,205 +168,12 @@ export default class App extends React.Component {
     this.sendKeyPress(e.keyCode, false);
   }
 
-
   render() {
     return (
       <div style={styles.canvasContainer}>
         <canvas id="ctx" style={styles.canvas} display="inline" width={window.innerWidth - 20} height={window.innerHeight - 20} margin={0} />
       </div>
     );
-  }
-
-  generateJunk() {
-    const newCoords = this.generateCoords(JUNK_COUNT);
-    newCoords.forEach((coord) => {
-      const props = {
-        position: { x: coord.x, y: coord.y },
-        canvas: this.canvas,
-      };
-      this.state.junk.push(new Junk(props));
-    });
-    this.setState(this.state);
-  }
-
-  generateHoles() {
-    const newCoords = this.generateCoords(HOLE_COUNT);
-    const newHoles = [];
-    newCoords.forEach((coord) => {
-      const props = {
-        position: { x: coord.x, y: coord.y },
-        radius: Math.floor(Math.random() * ((MAX_HOLE_RADIUS - MIN_HOLE_RADIUS) + 1)) + MIN_HOLE_RADIUS,
-        lifespan: Math.floor(Math.random() * ((MAX_HOLE_LIFE - MIN_HOLE_LIFE) + 1)) + MIN_HOLE_LIFE,
-        canvas: this.canvas,
-      };
-      const hole = new Hole(props);
-      newHoles.push(hole);
-    });
-    this.setState({
-      holes: newHoles,
-    });
-  }
-
-  // TODO check for collisions
-  generatePlayerCoords() {
-    const maxWidth = (2 * width) / 3;
-    const minWidth = width / 3;
-    const maxHeight = (2 * height) / 3;
-    const minHeight = height / 3;
-    const x = Math.floor(Math.random() * ((maxWidth - minWidth) + 1)) + minWidth;
-    const y = Math.floor(Math.random() * ((maxHeight - minHeight) + 1)) + minHeight;
-    return {x, y};
-  }
-
-  // should appear somewhere in the centre
-  generatePlayer() {
-    const coords = this.generatePlayerCoords();
-    const props = {
-      x: coords.x,
-      y: coords.y,
-      canvas: this.canvas,
-      theta: 0,
-    };
-    this.state.allCoords.push({ x: coords.x, y: coords.y });
-    const player = new Player(props);
-    this.setState({
-      player,
-      allCoords: this.state.allCoords,
-    });
-  }
-
-  generateCoords(num) {
-    // make sure object radius isn't outside of canvas
-    const maxWidth = width - MAX_DISTANCE_BETWEEN;
-    const minWidth = MAX_DISTANCE_BETWEEN;
-    const maxHeight = height - MAX_DISTANCE_BETWEEN;
-    const minHeight = MAX_DISTANCE_BETWEEN;
-
-    let count = num;
-    const coords = [];
-    while (count > 0) {
-      const x = Math.floor(Math.random() * ((maxWidth - minWidth) + 1)) + minWidth;
-      const y = Math.floor(Math.random() * ((maxHeight - minHeight) + 1)) + minHeight;
-      const isColliding = this.state.allCoords.some((p) => {
-        return areCirclesColliding(p.x, p.y, MAX_DISTANCE_BETWEEN, x, y, MAX_DISTANCE_BETWEEN);
-      });
-
-      if (!isColliding) {
-        const newAllCoords = this.state.allCoords.push({ x, y });
-        this.setState({ allCoords: newAllCoords });
-        coords.push({ x, y });
-        count -= 1;
-      }
-    }
-    return coords;
-  }
-
-  generateNewHoleCoords() {
-    // make sure object radius isn't outside of canvas
-    const maxWidth = width - MAX_DISTANCE_BETWEEN;
-    const minWidth = MAX_DISTANCE_BETWEEN;
-    const maxHeight = height - MAX_DISTANCE_BETWEEN;
-    const minHeight = MAX_DISTANCE_BETWEEN;
-
-    const coords = { x: 0, y: 0 };
-    while (true) {
-      coords.x = Math.floor(Math.random() * ((maxWidth - minWidth) + 1)) + minWidth;
-      coords.y = Math.floor(Math.random() * ((maxHeight - minHeight) + 1)) + minHeight;
-
-      let isColliding = false;
-      this.state.holes.forEach((hole) => {
-        const { position } = hole;
-        // Check every other
-        if (areCirclesColliding(position, MAX_HOLE_RADIUS, coords, MAX_HOLE_RADIUS)) {
-          isColliding = true;
-        }
-      });
-      this.state.junk.forEach((junk) => {
-        const { position } = junk;
-        // Check every junk so we don't swallow them up
-        if (areCirclesColliding(position, JUNK_SIZE, coords, MAX_HOLE_RADIUS)) {
-          isColliding = true;
-        }
-      });
-      // Check player to junk collisions
-      if (this.state.player && !isColliding) {
-        if (areCirclesColliding(this.state.player.position, PLAYER_RADIUS * 3, coords, MAX_HOLE_RADIUS)) {
-          isColliding = true;
-        }
-      }
-
-      // Dangerous infite loop?
-      if (!isColliding) {
-        break;
-      }
-    }
-    return coords;
-  }
-
-  
-  resizeCanvas() {
-    const ctx = document.getElementById('ctx');
-    ctx.width = window.innerWidth - 20;
-    ctx.height = window.innerHeight - 20;
-    ctx.textAlign = 'center';
-    this.updateCanvas();
-  }
-
-  checkForCollisions() {
-    // Check hole to player/junk collisions
-    this.state.holes.forEach((hole) => {
-      const { position, radius } = hole;
-      // Check the player
-      if (this.state.player) {
-        if (areCirclesColliding(this.state.player.position, PLAYER_RADIUS, position, radius)) {
-          this.setState({
-            player: null,
-          });
-        }
-      }
-
-      // Check each junk
-      this.state.junk.forEach((junk) => {
-        if (areCirclesColliding(junk.position, JUNK_SIZE, position, radius)) {
-          // Add points for the last bumper player here
-          if (junk.lastHitBy !== null) {
-            this.state.player.points += POINTS_PER_JUNK;
-          }
-
-          this.state.junk = this.state.junk.filter(j => j !== junk);
-          this.setState(this.state);
-        }
-      });
-    });
-
-    // Check player to junk collisions
-    this.state.junk.forEach((junk) => {
-      const { position } = junk;
-      if (this.state.player) {
-        if (areCirclesColliding(this.state.player.position, PLAYER_RADIUS, position, JUNK_SIZE)) {
-          junk.hitBy(this.state.player);
-        }
-      }
-    });
-  }
-
-  updateCanvas() {
-    const ctx = this.canvas.getContext('2d');
-    ctx.clearRect(0, 0, width, height);
-    this.drawJunk();
-    this.drawHoles();
-    this.drawPlayers();
-    this.drawPlayerPoints();
-  }
-
-  calculateNextState() {
-    // TODO check all players
-    if (!this.state.player) {
-      return;
-    }
-
-    this.state.player.updatePosition();
-    this.state.junk.forEach(j => j.updatePosition());
   }
 }
 

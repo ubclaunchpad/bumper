f3ce24610b1d51b33ee6288b8e21d46cd3ecc514
Amanda Lazar <amandalazar@shaw.ca>
2018-03-16 12:15:56 -0700

    Leaderboard implemented

--- client/App.js
+++ client/App.js
@@ -3,6 +3,12 @@ import Player from './components/Player';
 import Hole from './components/Hole';
 import Junk from './components/Junk';
 
+const DRAW = 0;
+const CLEAR = 1;
+const NUM_RANKS = 6;
+
+let printedPlayerRank = false;
+
 const PLAYER_RADIUS = 25;
 const JUNK_COUNT = 10;
 const JUNK_SIZE = 15;
@@ -45,9 +51,14 @@ export default class App extends React.Component {
       isInitialized: false,
       junk: null,
       holes: null,
-      players: null,
-      player: null,
-      points: 0,
+      players: ["Player A","Player B","Player C","Player D","Player E", "Player F", "Player G"],
+      currPlayer: "Player G",
+      currPlayerPoints: 100,
+      playerRank: 7, // This should be calculated based on an iteration through points
+      allPoints: [700,600,500,400,300,200,100],
+      topFivePlayers: ["Player A","Player B","Player C","Player D","Player E"],
+      topFivePoints: [700,600,500,400,300,200,100],
+      playerColor: '#1702ff',
     };
 
     this.handleMessage = this.handleMessage.bind(this);
@@ -99,6 +110,79 @@ export default class App extends React.Component {
     // eslint-disable-next-line
     requestAnimationFrame(this.tick);
   }
+
+  /*
+   * Performs on operation on the leaderboard 
+   * Param operation: CLEAR clears the leaderboard from the black canvas
+   *                  DRAW draws an updated leaderboard on the canvas
+   * Requires: topFivePlayers[], an array with the name strings of the top five players, in order from 1 to 5
+   *           playerRank, an int of the rank of the player
+   *           currPlayer, a string of the player's name
+   *           playerColor, some representation of the player's color
+   *           topFivePoints[], an array of ints corresponding to points of the top five players, in order from 1 to 5
+   *           currPlayerPoints, an int of the points the player currently has
+   */
+  leaderboard(operation) {
+    // Draw the rectangle for the leaderboard:
+	  const ctx = this.canvas.getContext('2d');
+    ctx.beginPath();
+    const rectHeight = 130;
+    const rectWidth = 170;
+    const rectX = window.innerWidth - rectWidth;
+    const rectY = 0;
+    ctx.rect(rectX, rectY, rectWidth, rectHeight);
+    if(operation == DRAW) {
+      ctx.fillStyle = 'rgba(255,0,0,0.3)';
+    } else if(operation == CLEAR) {
+      ctx.fillStyle = '#000000';
+    }
+    ctx.fill();
+    
+    // Print leaderboard data:
+    if(operation == DRAW) {
+      // Draw the leaderboard title:
+      ctx.font = '16px Lucida Sans Unicode';
+      ctx.textAlign = 'center'; 
+      ctx.textBaseline = 'middle';
+      ctx.fillStyle = '#FFFFFF';
+      ctx.fillText('Leaderboard', rectX + (rectWidth / 2) - 10, rectY + (rectHeight / 2) - 45);
+      
+      // Draw the ranks with corresponding player names and points:
+      ctx.font = '10px Lucida Sans Unicode';
+      let index;
+      for(let currRank = 1; currRank < NUM_RANKS; currRank++) {
+        index = currRank - 1;
+        printedPlayerRank = false;
+
+        if(this.state.playerRank == currRank) {  // If player is in the top 5, print its rank in its player color
+          printedPlayerRank = true;
+          ctx.fillStyle = this.state.playerColor;
+          ctx.textAlign = 'left'; 
+          ctx.fillText(rank + '. ' + this.state.topFivePlayers[index], rectX + (rectWidth / 2) - 80, rectY + (rectHeight / 2) - 25 + 15 * index);
+          ctx.textAlign = 'right';
+          ctx.fillText(this.state.topFivePoints[index], rectX + (rectWidth / 2) + 60, rectY + (rectHeight / 2) - 25 + 15 * index);
+          ctx.fillStyle = '#FFFFFF';
+        }
+        else {   // Else, just print the rank
+          ctx.textAlign = 'left'; 
+          ctx.fillText(currRank + '. ' + this.state.topFivePlayers[index], rectX + (rectWidth / 2) - 80, rectY + (rectHeight / 2) - 25 + 15 * index);
+          ctx.textAlign = 'right';
+          ctx.fillText(this.state.topFivePoints[index], rectX + (rectWidth / 2) + 60, rectY + (rectHeight / 2) - 25 + 15 * index);
+        }
+      }
+      if(!printedPlayerRank) { // Print the player's rank if it hasn't already been printed
+        index = NUM_RANKS - 1;
+        ctx.fillStyle = this.state.playerColor;
+        ctx.textAlign = 'left'; 
+        ctx.fillText(this.state.playerRank + '. ' + this.state.currPlayer, rectX + (rectWidth / 2) - 80, rectY + (rectHeight / 2) - 25 + 15 * index);
+        ctx.textAlign = 'right';
+        ctx.fillText(this.state.currPlayerPoints, rectX + (rectWidth / 2) + 60, rectY + (rectHeight / 2) - 25 + 15 * index);
+        ctx.fillStyle = '#FFFFFF';
+      }
+
+    }
+    ctx.closePath();
+  }
   
   drawHoles() {
     this.state.holes.forEach((h) => {
@@ -133,30 +217,13 @@ export default class App extends React.Component {
       ctx.closePath();
     });
   }
-  
-  drawPlayerPoints() {
-    const ctx = this.canvas.getContext('2d');
-    ctx.beginPath();
-    const rectHeight = 40;
-    const rectWidth = 150;
-    const rectX = window.innerWidth - 150;
-    const rectY = 0;
-    ctx.rect(rectX, rectY, rectWidth, rectHeight);
-    ctx.fillStyle = this.state.player.color;
-    ctx.fill();
-    ctx.font = '16px Lucida Sans Unicode';
-    ctx.textAlign = 'center'; 
-    ctx.textBaseline = 'middle';
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fillText(`Points: ${this.state.player.points}`, rectX + (rectWidth / 2) - 10, rectY + (rectHeight / 2) + 2);
-    ctx.closePath();
-  }
 
   draw() {
     this.drawHoles();
     this.drawJunk();
     this.drawPlayers();
-    this.drawPlayerPoints();
+    this.leaderboard(CLEAR);
+    this.leaderboard(DRAW);
   }
 
   render() {

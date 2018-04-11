bc4d982a36d0f4b0c4fff8db90e06e8b1b8331c5
Tommy Lau <tommylau94@gmail.com>
2018-04-11 14:03:37 -0700

    add player model rocket chassis

--- client/App.js
+++ client/App.js
@@ -289,28 +289,77 @@ export default class App extends React.Component {
     this.state.players.forEach((p) => {
       const ctx = this.canvas.getContext('2d');
       const { x, y } = p.position;
+
+      //Constants
+      const sinAngle = Math.sin(p.angle);
+      const cosAngle = Math.cos(p.angle);
+
+      // const frontCenterX = x + (PLAYER_RADIUS * sinAngle);
+      // const frontCenterY = y + (PLAYER_RADIUS * cosAngle);
+
+      const backCenterX = x - ((PLAYER_RADIUS * sinAngle) / 4*3);
+      const backCenterY = y - ((PLAYER_RADIUS * cosAngle) / 4*3);
+
+      const backLength = ((PLAYER_RADIUS / 2));
+
+      //TESTING
+      //Circle
+      // ctx.beginPath();
+      // ctx.arc(x, y, PLAYER_RADIUS, 0, Math.PI * 2);
+      // ctx.fillStyle = '#FFFFFF';
+      // ctx.fill();
+      // ctx.closePath();
+
+      //TESTING
+      //Center Line
+      // ctx.beginPath();
+      // ctx.moveTo(frontCenterX, frontCenterY);
+      // ctx.lineTo(x - (PLAYER_RADIUS * sinAngle), y - (PLAYER_RADIUS * cosAngle));
+      // ctx.strokeStyle = '#FFFFFF';
+      // ctx.strokeWidth = 5;
+      // ctx.stroke();
+
+      /*
+      Start drawing Rocket Chassis, starts from bottom right to the bottom left,
+      draw toward the rocket tip then back to the bottom right to complete the shape and fill
+      */
+      //Coordinates of the Rocket Tip
+      const rocketTipX = x + (PLAYER_RADIUS * sinAngle)*1.2;
+      const rocketTipY = y + (PLAYER_RADIUS * cosAngle)*1.2;
+      //Control Points for Bezier Curve from/toward the Rocket Tip
+      const rocketTipModifierRightX = x + (PLAYER_RADIUS * Math.sin(p.angle - Math.PI / 4 ));
+      const rocketTipModifierRightY = y + (PLAYER_RADIUS * Math.cos(p.angle - Math.PI / 4 ));
+      const rocketTipModifierLeftX = x + (PLAYER_RADIUS  * Math.sin(p.angle + Math.PI / 4 ));
+      const rocketTipModifierLeftY = y + (PLAYER_RADIUS  * Math.cos(p.angle + Math.PI / 4 ));
+      //Center-Right Coordinates of Rocket   
+      const rightCenterX = x + (PLAYER_RADIUS * Math.sin(p.angle - Math.PI / 2 ));
+      const rightCenterY = y + (PLAYER_RADIUS * Math.cos(p.angle - Math.PI / 2 ));
+      //Center-Left Coordinates of Rocket
+      const leftCenterX = x + (PLAYER_RADIUS  * Math.sin(p.angle + Math.PI / 2 ));
+      const leftCenterY = y + (PLAYER_RADIUS  * Math.cos(p.angle + Math.PI / 2 ));
+      //Base Coordinates
+      const rocketBottomRightX = backCenterX - (backLength * cosAngle);
+      const rocketBottomRightY = backCenterY + (backLength * sinAngle);
+      const rocketBottomLeftX = backCenterX + (backLength * cosAngle);
+      const rocketBottomLeftY = backCenterY - (backLength * sinAngle);
+      //Rocket Base
       ctx.beginPath();
-      ctx.arc(x, y, PLAYER_RADIUS, 0, Math.PI * 2);
+      ctx.moveTo(rocketBottomRightX, rocketBottomRightY); //bottom right side
+      ctx.lineTo(rocketBottomLeftX, rocketBottomLeftY); //bottom left side
+      //Left Side
+      ctx.bezierCurveTo(leftCenterX, leftCenterY, rocketTipModifierLeftX, rocketTipModifierLeftY, rocketTipX, rocketTipY); //chassis left side
+      //Right Side
+      ctx.bezierCurveTo(rocketTipModifierRightX, rocketTipModifierRightY, rightCenterX, rightCenterY, rocketBottomRightX, rocketBottomRightY); //chassis right side
       ctx.fillStyle = p.color;
       ctx.fill();
       ctx.closePath();
 
-      ctx.beginPath();
-      ctx.moveTo(x + (PLAYER_RADIUS * Math.sin(p.angle)), y + (PLAYER_RADIUS * Math.cos(p.angle)));
-      ctx.lineTo(x - (PLAYER_RADIUS * Math.sin(p.angle)), y - (PLAYER_RADIUS * Math.cos(p.angle)));
-      ctx.strokeStyle = '#000000';
-      ctx.strokeWidth = 5;
-      ctx.stroke();
+      //TODO: Rocket Right Wing
+      //TODO: Rocket Left Wing
+      //TODO: Rocket Bottom piece
+      //TODO: Rocket Window
 
-      const backCenterX = x - ((PLAYER_RADIUS * Math.sin(p.angle)) / 2);
-      const backCenterY = y - ((PLAYER_RADIUS * Math.cos(p.angle)) / 2);
-      const backLength = (2.5 * ((PLAYER_RADIUS / 2) / Math.tan(45)));
-      ctx.beginPath();
-      ctx.moveTo(backCenterX - (backLength * Math.cos(p.angle)), backCenterY + (backLength * Math.sin(p.angle)));
-      ctx.lineTo(backCenterX + (backLength * Math.cos(p.angle)), backCenterY - (backLength * Math.sin(p.angle)));
-      ctx.strokeStyle = '#0000000';
-      ctx.strokeWidth = 5;
-      ctx.stroke();
+
     });
   }
 

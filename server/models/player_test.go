a261e9d82b6b20ab44daefd34840c841004e2971
Tommy Lau <tommylau94@gmail.com>
2018-05-14 01:12:50 -0700

    some tests

--- server/models/player_test.go
+++ server/models/player_test.go
@@ -1,6 +1,10 @@
 package models
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/gorilla/websocket"
+)
 
 func TestAddPoints(t *testing.T) {
 	p := new(Player)
@@ -8,5 +12,48 @@ func TestAddPoints(t *testing.T) {
 	p.AddPoints(10)
 	if p.Points != 10 {
 		t.Error("Error adding points")
+	}
+}
+
+func TestCreatePlayer(t *testing.T) {
+	testPosition := Position{5, 5}
+	testName := "testy"
+	testColor := "blue"
+	ws := new(*websocket.Conn)
+	p := CreatePlayer(testName, testPosition, testColor, *ws)
+
+	//Name Assignment
+	if p.Name != testName {
+		t.Error("Error assigning name")
+	}
+
+	//To do: test other initializations
+}
+
+func TestKeyDownHandler(t *testing.T) {
+
+}
+
+func TestKeyUpHandler(t *testing.T) {
+	p := new(Player)
+	key := UpKey
+	p.KeyUpHandler(key)
+	if p.Controls.Up != false {
+		t.Error("Error handling up key control")
+	}
+	key = RightKey
+	p.KeyUpHandler(key)
+	if p.Controls.Right != false {
+		t.Error("Error handling right key control")
+	}
+	key = DownKey
+	p.KeyUpHandler(key)
+	if p.Controls.Down != false {
+		t.Error("Error handling down key control")
+	}
+	key = LeftKey
+	p.KeyUpHandler(key)
+	if p.Controls.Left != false {
+		t.Error("Error handling left key control")
 	}
 }

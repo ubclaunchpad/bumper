a5e90ab3fe52828f63af56c3876c431c196b71cc
brian-nguyen <briannguyen992@gmail.com>
2018-05-29 18:47:43 -0700

    Deriving production config from dev

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,29 +1,7 @@
 /* eslint-disable */
-const webpack = require('webpack');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
 
-const config = {
-  entry: './index.js',
-  output: {
-    path: `${__dirname}/public/`,
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: '/node_modules/',
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
-        },
-      },
-    ],
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin(['NODE_ENV']),
-    new MinifyPlugin(),
-  ]
-};
+const config = require('./webpack.config');
+config.plugins.push(new MinifyPlugin);
 
-module.exports = env => config;
+module.exports = config;
